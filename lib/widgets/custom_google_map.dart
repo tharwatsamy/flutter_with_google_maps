12c5780531bf4ddd605eae736b1cbd8383e47092
tharwatsamy <tharwatsamy2004@gmail.com>
2024-01-23 23:26:47 +0200

    check and request location service

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class CustomGoogleMap extends StatefulWidget {
   const CustomGoogleMap({super.key});
@@ -12,10 +13,14 @@ class CustomGoogleMap extends StatefulWidget {
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPostion;
 
+  late Location location;
   @override
   void initState() {
     initialCameraPostion = const CameraPosition(
         zoom: 17, target: LatLng(31.187084851056554, 29.928110526889437));
+
+    location = Location();
+    checkAndRequestLocationService();
 
     super.initState();
   }
@@ -40,9 +45,19 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
 
     googleMapController.setMapStyle(nightMapStyle);
   }
+
+  void checkAndRequestLocationService() async {
+    var isServiceEnabled = await location.serviceEnabled();
+    if (!isServiceEnabled) {
+      isServiceEnabled = await location.requestService();
+      if (!isServiceEnabled) {
+        // TODO: show error bar
+      }
+    }
+  }
 }
-// world view 0 -> 3
-// country view 4-> 6
-// city view 10 -> 12
-// street view 13 -> 17
-// building view 18 -> 20
+
+// inquire about location service
+// request permission
+// get location
+// display
