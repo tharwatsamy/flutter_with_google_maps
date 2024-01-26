33976da4de70996bb73bb1fc17d5b8f4d55e95d1
tharwatsamy <tharwatsamy2004@gmail.com>
2024-01-26 13:06:16 +0200

    create location service

---
+++ lib/utils/location_service.dart
@@ -0,0 +1,31 @@
+import 'package:location/location.dart';
+
+class LocationService {
+  Location location = Location();
+
+  Future<bool> checkAndRequestLocationService() async {
+    var isServiceEnabled = await location.serviceEnabled();
+    if (!isServiceEnabled) {
+      isServiceEnabled = await location.requestService();
+      if (!isServiceEnabled) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  Future<bool> checkAndRequestLocationPermission() async {
+    var permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.deniedForever) {
+      return false;
+    }
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await location.requestPermission();
+
+      return permissionStatus == PermissionStatus.granted;
+    }
+
+    return true;
+  }
+}
